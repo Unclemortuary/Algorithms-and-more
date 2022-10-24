1d455bcc109771d580f384bccec498b9e4134740
Unclemortuary <artem_mihalych@mail.ru>
2022-10-25 01:08:49 +0300

    quickSort tests

---
+++ sortings/index.test.js
@@ -0,0 +1,21 @@
+const quickSort = require('./index.js');
+
+describe('quickSort', () => {
+    test('unordered urray gets ordered', () => {
+        const input = [9,8,7,6,5,4,3,2];
+        const expected = [2,3,4,5,6,7,8,9];
+        expect(quickSort(input, 1)).toEqual(expected);
+    });
+
+    test('all the same elements', () => {
+        const input = [5,5,5,5];
+        const expected = [5,5,5,5];
+        expect(quickSort(input, 1)).toEqual(expected);
+    });
+
+    test('non array input, throws error', () => 
+        expect(() => 
+            { quickSort('12314eqw') })
+            .toThrow()
+    );
+});
